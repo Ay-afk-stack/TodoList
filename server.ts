869f211676850f2de271b28e5246f51e4faf00ff
Ay-afk-stack <pakhrin.ayush404@gmail.com>
2024-11-09 20:55:09 +0545

    Simple Event Tests

--- server.ts
+++ server.ts
@@ -3,6 +3,11 @@ import envConfig from "./src/config/config";
 import connecttoDatabase from "./src/database/connection";
 import { Server } from "socket.io";
 
+//post-on
+//get-emit
+//request-socket
+//routing-event
+
 const startServer = () => {
   const port = envConfig.port || 4000;
   const server = app.listen(port, () => {
@@ -10,7 +15,15 @@ const startServer = () => {
     console.log("Listening at http://localhost:" + port);
   });
   const io = new Server(server);
-  io.on("connection", () => {
+  io.on("connection", (socket) => {
+    // socket.on("haha", (data) => {
+    //   console.log(data);
+    //   socket.emit("response", { message: "Data received!" });
+    // });
+
+    socket.emit("login", {
+      message: "Login successful!",
+    });
     console.log("Someone connected (Client)");
   });
 };
